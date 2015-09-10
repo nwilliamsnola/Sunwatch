2a613db2df2c90994e0f682bc7b738025e27f1fc
“NWILLIAMSNOLAgit config --global user.email “NWILLIAMSNOLA@GMAIL.COM <NWILLIAMSNOLA@gmail.com>
2015-09-10 18:08:25 -0500

    added jquery helpers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  require 'jquery/modal/rails'
+require 'jquery/modal/filters'
+require 'jquery/modal/helpers'
 end
